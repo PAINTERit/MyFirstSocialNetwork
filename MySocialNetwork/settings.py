822c20da5070522fbc4c8af13a31f59ac911fd06
artem <artemv2001@mail.ru>
2023-04-25 19:49:00 +0300

    Добавлена возможность добавить фото в профиле.

--- MySocialNetwork/settings.py
+++ MySocialNetwork/settings.py
@@ -138,6 +138,8 @@ AUTH_USER_MODEL = 'cabinet.Person'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
