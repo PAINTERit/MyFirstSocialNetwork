7d149bcaa6840123f14f36cbdc96cf138c6e8903
artem <artemv2001@mail.ru>
2023-05-23 23:14:49 +0300

    Обновлены функции проверки и создания комнат. Обновлена модель комнат.

--- chat/models.py
+++ chat/models.py
@@ -3,7 +3,7 @@ from cabinet.models import Person
 
 
 class Room(models.Model):
-    room_id = models.CharField(max_length=50, unique=True)
+    room_id = models.CharField(max_length=50)
     user_id = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='users_id')
     friend_id = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='friends_id')
     created_at = models.DateTimeField(auto_now_add=True)
